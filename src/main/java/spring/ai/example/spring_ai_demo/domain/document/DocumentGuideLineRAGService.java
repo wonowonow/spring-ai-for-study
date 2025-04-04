2ee99bc1a91558058f09eb001366aa834954c47d
wonowonow <123870616+wonowonow@users.noreply.github.com>
2025-04-04 19:55:45 +0900

    feat: RAG 예시 작성 (근사값 및 LLM)

--- src/main/java/spring/ai/example/spring_ai_demo/domain/document/DocumentGuideLineRAGService.java
+++ src/main/java/spring/ai/example/spring_ai_demo/domain/document/DocumentGuideLineRAGService.java
@@ -1,20 +1,52 @@
 package spring.ai.example.spring_ai_demo.domain.document;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chroma.vectorstore.ChromaVectorStore;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
-@RequiredArgsConstructor
 public class DocumentGuideLineRAGService {
 
     private final DocumentVectorRepository documentVectorRepository;
+    private final ChatClient chatClient;
+    private final ChromaVectorStore chromaVectorStore;
+    private final ChatModel chatModel;
+
+    public DocumentGuideLineRAGService(DocumentVectorRepository documentVectorRepository, ChromaVectorStore chromaVectorStore, ChatClient chatClient, ChatModel chatModel) {
+        this.documentVectorRepository = documentVectorRepository;
+        this.chromaVectorStore = chromaVectorStore;
+        this.chatModel = chatModel;
+        this.chatClient = ChatClient.builder(chatModel)
+                                    .defaultAdvisors(
+                                            new QuestionAnswerAdvisor(
+                                                    chromaVectorStore, SearchRequest.builder()
+                                                                                    .similarityThreshold(0.8d)
+                                                                                    .topK(6).build()))
+                                    .build();
+    }
 
     public String classifyDocumentGuideLine(String documentType) {
-        return documentVectorRepository.findDocumentGuideLine(documentType);
+
+        log.info("documentType 에 따른 GuideLine RAG 검색");
+        String content = chatClient.prompt(new Prompt("documentType 에 따른 DocumentGuideLine 을 작성해주세요")).user(documentType).call().content();
+        log.info("content: {}", content);
+
+        return content;
     }
 
     public String classifyDocumentGuideLine(String documentType, String key) {
 
-        return documentVectorRepository.findDocumentGuideLine(documentType, key);
+        log.info("documentType 과 Key 에 따른 GuideLine RAG 검색");
+        String content = chatClient.prompt(new Prompt("documentType 과 Key 에 따라 Key 의 guideLine 을 작성해주세요")).user(documentType + ", " + key).call().content();
+        log.info("content: {}", content);
+
+        return content;
     }
 }
